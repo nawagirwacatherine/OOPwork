5bfef7d7e0b36f6dfb5af8a620b5858d868eb1e9
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-04-03 06:38:17 +0300

    done class bird

---
+++ excise.js
@@ -0,0 +1,24 @@
+class Bird{
+ useWings(){
+    console.log("Flying!")
+ }
+}
+
+class Eagle extends Bird {
+    useWings(){
+        super.useWings()
+        console.log("Barely flapping!")
+    }
+}
+
+class Penguin extends Bird {
+    useWings(){
+        console.log("Diving!")
+    }
+}
+
+var baldEagle = new Eagle();
+var KingPenguin = new penguin();
+
+baldEagle.useWings();
+KingPenguin.useWings();
