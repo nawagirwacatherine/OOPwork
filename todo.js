6591712394a6a92b2c7d7c3f56391202c0132b22
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-04-05 03:39:14 +0300

    added else statement

---
+++ todo.js
@@ -0,0 +1,12 @@
+var toDo =["todo1", "todo2"];
+if (toDo == toDo[0]){
+  console.log("wash dishes")
+}
+
+else if (toDo == toDo[1]){
+    console.log("buy groceries");
+}
+
+else {
+    console.log("click button");
+}
