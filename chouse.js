4d381cdc2ebb449bb56f1fc9d5af5bcbab3e1513
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-04-22 16:07:51 +0300

    built a class house

---
+++ chouse.js
@@ -0,0 +1,27 @@
+class house{
+    userooms(){
+        console.log("staying!")
+    }
+}
+
+class sitting extends house {
+    userooms(){
+        super.userooms()
+        console.log("visitors staying!")
+    }
+}
+    class kitchen extends house {
+        
+        userooms(){
+            
+            console.log("cooking!")
+        }
+    }
+    var sittingroom = new sitting();
+    var bigkitchen = new kitchen();
+
+    sittingroom.userooms();
+    bigkitchen.userooms();
+
+
+
