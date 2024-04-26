9b11d5a5a738e0bbd960b291b095477bcecaae1f
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-04-26 13:09:23 +0300

    done iterating over arrays

--- prac.js
+++ prac.js
@@ -1,3 +1,46 @@
-let greet = "Hello";
+/*let greet = "Hello";
 let place = "World";
-console.log(`${greet} ${place}!`);
+console.log(`${greet} ${place}!`);*/
+
+var dairy = ['cheese','sour cream','milk','yogurt','ice cream','milkshake'];
+  function logDiary(){
+    for (dairy of dairy){
+        console.log(dairy);
+    }
+  }
+  logDiary();
+
+  const animal ={
+
+    canJump: true
+  };
+
+  const bird = Object.create(animal);
+
+  bird.canFly = true;
+
+  bird.hasFeathers = true;
+
+  function birdCan(){
+    for (const entries of Object.entries(bird)){
+        console.log(bird);
+
+    }
+
+  }
+  birdCan();
+
+  
+function animalCan() {
+    for (const entries in Object.entries(animal)){
+     console.log(animal);
+    }
+
+}
+
+animalCan();
+
+
+
+
+  
