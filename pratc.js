940c334ca7729019add6602a255698ef1b571c07
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-05-24 14:57:19 +0200

    added HighSpeedTrain from the base class train

--- pratc.js
+++ pratc.js
@@ -18,10 +18,48 @@ class Train{
         var proto = Object.getPrototypeOf(this);
         console.log(proto);
     }
-
 }
+    class HighSpeedTrain extends Train {
+        constructor(passengers, highSpeedOn, color, lightsOn){
+            super(color,lightsOn);
+            this.passengers = passengers;
+            this.highSpeedOn = highSpeedOn;
+        }
+        toggleHighSpeed(){
+            this.highSpeedOn = !this.highSpeedOn;
+            console.log('High speed status:', this.highSpeedOn)
+        }
 
-var train1 =new Train("green",false);
-train1.togglelights();
-train1.getSelf();
-train1.getPrototype();
+        toggleLights(){
+            super.toggleLights();
+            super.lightsStatus();
+            console.log('Lights are 100% operational.');
+        }
+
+    }
+
+    var myFirstTrain = new Train('red', false);
+    console.log(myFirstTrain);
+    var mySecondTrain = new Train('blue',false);
+    var myThirdTrain = new Train('blue',false);
+
+
+    var train4 = new Train('red',false);
+    train4.togglelights();
+    train4.lightsStatus();
+    train4.getSelf();
+    train4.getPrototype();
+
+
+    var train5 = new Train('blue',false);
+    var highSpeed1 =new HighSpeedTrain(200,false,'green',false);
+
+    train5.togglelights();
+    train5.lightsStatus();
+    highSpeed1.togglelights();
+
+
+
+
+
+
